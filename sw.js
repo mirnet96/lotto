31d8e97333219d210c9e529a4e31bbb1fc69545c
mirnet96 <mirnet96@naver.com>
2026-05-23 21:41:54 +0900

    pwa modify

--- sw.js
+++ sw.js
@@ -3,22 +3,28 @@ const ASSETS = [
   '/lotto/',
   '/lotto/index.html',
   '/lotto/style.css',
-  '/lotto/js/state.js' 
-  '/lotto/js/storage.js' 
-  '/lotto/js/ui.js' 
-  '/lotto/js/generator.js' 
-  '/lotto/js/qr.js' 
-  '/lotto/js/history.js' 
-  '/lotto/js/stats.js' 
-  '/lotto/js/init.js' 
-
-
+  '/lotto/js/init.js',
+  '/lotto/js/state.js',
+  '/lotto/js/storage.js',
+  '/lotto/js/ui.js',
+  '/lotto/js/generator.js',
+  '/lotto/js/qr.js',
+  '/lotto/js/history.js',
+  '/lotto/js/stats.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
